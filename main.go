8bf7bb8b9134ff9d6515e413e94da1f08eb814bb
“donajivt” <vaniadonajitorres@gmail.com>
2025-06-03 14:19:00 -0600

    Se agrego una nueva ruta para poder consumir el microservicio desde esta

--- main.go
+++ main.go
@@ -33,7 +33,7 @@ func main() {
 	router := gin.Default()
 
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173"}, // Origen del frontend
+		AllowOrigins:     []string{"http://localhost:5173", "http://localhost:5174"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
